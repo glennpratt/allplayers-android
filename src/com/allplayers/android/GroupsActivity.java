197736d47c1aa1f267c4e75ee0cf5fc65b79846f
Glenn Pratt <glennpratt@gmail.com>
2011-12-28 09:45:07 -0600

    Issue #67: Move GroupsActivity loading to the background, just a start.

--- src/com/allplayers/android/GroupsActivity.java
+++ src/com/allplayers/android/GroupsActivity.java
@@ -5,6 +5,7 @@ import com.allplayers.rest.RestApiV1;
 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
@@ -22,41 +23,17 @@ public class GroupsActivity extends ListActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (Globals.groupList.isEmpty()) {
-            String jsonResult = "";
+        // Load the list in a background task.
+        new GroupsTask().execute();
 
-            //check local storage
-            if (LocalStorage.getTimeSinceLastModification("UserGroups") / 1000 / 60 < 60) { //more recent than 60 minutes
-                jsonResult = LocalStorage.readUserGroups(getBaseContext());
-            } else {
-                jsonResult = RestApiV1.getUserGroups();
-                LocalStorage.writeUserGroups(getBaseContext(), jsonResult, false);
-            }
+    }
 
-            GroupsMap groups = new GroupsMap(jsonResult);
-            groupList = groups.getGroupData();
-            Globals.groupList = groupList;
-        } else {
-            groupList = Globals.groupList;
-        }
-
-        String[] values;
-
-        if (!groupList.isEmpty()) {
-            values = new String[groupList.size()];
-
-            for (int i = 0; i < groupList.size(); i++) {
-                values[i] = groupList.get(i).getTitle();
-            }
-
-            hasGroups = true;
-        } else {
-            values = new String[] {"no groups to display"};
-            hasGroups = false;
-        }
-
+    /**
+     * Helper to set the list content from an array.
+     */
+    protected void setListContent(String[] listContent) {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, values);
+                android.R.layout.simple_list_item_1, listContent);
         setListAdapter(adapter);
     }
 
@@ -80,5 +57,75 @@ public class GroupsActivity extends ListActivity {
         }
 
         return super.onKeyUp(keyCode, event);
+    }
+    /**
+     * Background task to load groups...
+     */
+    private class GroupsTask extends AsyncTask<String, String, ArrayList<GroupData>> {
+        /**
+         * Before jumping into background thread, start busy animation.
+         */
+        @Override
+        protected void onPreExecute() {
+            // TODO: Add busy animation.
+        }
+
+        /**
+         * Perform the background query using {@link ExtendedWikiHelper}, which
+         * may return an error message as the result.
+         */
+        @Override
+        protected ArrayList<GroupData> doInBackground(String... args) {
+            if (Globals.groupList.isEmpty()) {
+                String jsonResult = "";
+
+                //check local storage
+                if (LocalStorage.getTimeSinceLastModification("UserGroups") / 1000 / 60 < 60) { //more recent than 60 minutes
+                    jsonResult = LocalStorage.readUserGroups(getBaseContext());
+                } else {
+                    jsonResult = RestApiV1.getUserGroups();
+                    LocalStorage.writeUserGroups(getBaseContext(), jsonResult, false);
+                }
+
+                GroupsMap groups = new GroupsMap(jsonResult);
+                groupList = groups.getGroupData();
+                Globals.groupList = groupList;
+            } else {
+                groupList = Globals.groupList;
+            }
+
+            return groupList;
+        }
+
+        /**
+         * Progress update (needs research).
+         */
+        @Override
+        protected void onProgressUpdate(String... args) {
+            // TODO: Update busy animation.
+        }
+
+        /**
+         * Finished, put the content in.
+         */
+        @Override
+        protected void onPostExecute(ArrayList<GroupData> groupList) {
+            // TODO: Stop busy animation.
+            String[] values;
+
+            if (!groupList.isEmpty()) {
+                values = new String[groupList.size()];
+
+                for (int i = 0; i < groupList.size(); i++) {
+                    values[i] = groupList.get(i).getTitle();
+                }
+
+                hasGroups = true;
+            } else {
+                values = new String[] {"no groups to display"};
+                hasGroups = false;
+            }
+            setListContent(values);
+        }
     }
 }
